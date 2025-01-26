1fc04bf0ee1eec43757e534b206198d55529cd8a
Shananu Soni <shantanu.soni.che13@itbhu.ac.in>
2025-01-26 21:30:20 +0000

    uvicorn setup

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
 import pandas as pd
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from pathlib import Path
 
 
 app = FastAPI()
+
+
+
+
+# Initialize FastAPI and Jinja2 Templates
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/", response_class=HTMLResponse)
+async def read_root():
+    return templates.TemplateResponse("home-page.html", 
+                                      {"request": {}})
 
 
 if __name__ == "__main__":
