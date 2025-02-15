6c9243a8f6d98f25906e8eb53e8d52e1dca95816
Shananu Soni <shantanu.soni.che13@itbhu.ac.in>
2025-02-15 14:29:27 +0000

    login page commit

--- static/js/login-page.js
+++ static/js/login-page.js
@@ -6,3 +6,56 @@ function togglePassword() {
         passwordField.type = "password";
     }
 }
+
+
+let scene, camera, renderer, particles;
+
+function init() {
+    scene = new THREE.Scene(); 
+    scene.background = new THREE.Color(0xffffff);
+    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    camera.position.z = 3;
+
+    renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    $('.ambience').append(renderer.domElement);
+
+    const particleGeometry = new THREE.BufferGeometry();
+    const particleCount = 1000;
+    const positions = new Float32Array(particleCount * 3);
+
+    for (let i = 0; i < particleCount * 3; i += 3) {
+        let phi = Math.acos(2 * Math.random() - 1);
+        let theta = Math.random() * Math.PI * 2;
+        let r = 1.5;
+        
+        positions[i] = r * Math.sin(phi) * Math.cos(theta);
+        positions[i + 1] = r * Math.sin(phi) * Math.sin(theta);
+        positions[i + 2] = r * Math.cos(phi);
+    }
+
+    particleGeometry.setAttribute('position', new THREE.BufferAttribute(positions, 3));
+    const particleMaterial = new THREE.PointsMaterial({ color: "#8A2BE2", size: 0.02 }); // Neon Green
+    particles = new THREE.Points(particleGeometry, particleMaterial);
+    scene.add(particles);
+
+    animate();
+}
+
+function animate() {
+    requestAnimationFrame(animate);
+    particles.rotation.y += 0.002;
+    renderer.render(scene, camera);
+}
+
+window.addEventListener('resize', () => {
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+});
+
+
+window.onload = function () {
+    init();
+};
+
